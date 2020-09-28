9ab76809a632b6673c3f8bd4d09e9b293fd22654
EmilioRui <63071086+EmilioRui@users.noreply.github.com>
2020-09-28 21:30:20 +0200

    aggiunto il try

--- Scraping-Chaptch.py
+++ Scraping-Chaptch.py
@@ -53,97 +53,101 @@ def main():
         reader = csv.DictReader(file)
         count = 0
         for row in reader:
-            if count == 0:
-                print("Pagina Caricata")
-                count += 1
+            try:
+                if count == 0:
+                    print("Pagina Caricata")
+                    count += 1
 
-            proxies = {
-                "http": "http://" + row['User'] + ":" + row['Auth'] + "@" + row['IP'] + ":" + row['Port'] + "/",
-                "https": "http://" + row['User'] + ":" + row['Auth'] + "@" + row['IP'] + ":" + row['Port'] + "/"
-            }
-
-            headers1 = {
-                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
-            }
-
-            with requests.Session() as session:
-                url = 'https://www.statuto18.com/DunkHighMaizeBlue'
-
-                res = requests.get(url, proxies=proxies, headers=headers1)
-                soup = BeautifulSoup(res.text, 'html.parser')
-
-                day = soup.select_one('input.form-control:nth-child(3)').get('name')
-                month = soup.select_one('input.form-control:nth-child(4)').get('name')
-                year = soup.select_one('input.form-control:nth-child(5)').get('name')
-                cf = soup.select_one('input.form-control:nth-child(9)').get('name')
-
-                if not day or not month or not year or not cf:
-                    print("Empty field: day/month/year/cf")
-                    continue
-
-
-                # solver = recaptchaV2Proxyless()
-                # solver.set_verbose(1)
-                # solver.set_key("bef80b4f70976b0452b83cddf6f9b152")
-                # solver.set_website_url(url)
-                # solver.set_website_key("6LdrtrgUAAAAAAAio3UhHrVdJUQXpP3vfbcFm3qx")
-                # g_response = solver.solve_and_return_solution()
-                # if g_response != 0:
-                #     print("g-response: " + g_response)
-                # else:
-                #     print("task finished with error " + solver.error_code)
-
-                while queue.empty():
-                    time.sleep(0.1)
-                g_response = queue.get()
-                form_data = {
-                    'nome2': row['Nome'],
-                    'cognome2': row['Cognome'],
-                    day: row['Giorno'],
-                    month: row['Mese'],
-                    year: row['Anno'],
-                    'telefono2': row['Telefono'],
-                    'email2': row['Mail'],
-                    'email_conf2': row['Mail'],
-                    cf: row['CF'],
-                    'comune2': row['Comune'],
-                    'provincia2': row['Provincia'],
-                    'paese2': 'IT',
-                    'sesso2': row['Sesso'],
-                    'taglia2': row['Taglia'],
-                    'check2': 'S',
-                    'captcha2': 'undefined',
-                    'campagna2': 'DunkHighMaizeBlue',
-                    'form': 'registra',
-                    'mark': 'S',
-                    'cessione': 'S',
-                    'g-recaptcha-response': g_response,
-                    'action': 'trueins'
+                proxies = {
+                    "http": "http://" + row['User'] + ":" + row['Auth'] + "@" + row['IP'] + ":" + row['Port'] + "/",
+                    "https": "http://" + row['User'] + ":" + row['Auth'] + "@" + row['IP'] + ":" + row['Port'] + "/"
                 }
 
-                m = MultipartEncoder(fields=form_data, boundary='----WebKitFormBoundary' + ''.join(
-                    random.sample(string.ascii_letters + string.digits, 16)))
-
-                headers2 = {
-                    "Content-Type": m.content_type,
-                    'Cookie': 'PHPSESSID=' + res.cookies.get('PHPSESSID'),
+                headers1 = {
                     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
                 }
 
-                # print(form_data)
+                with requests.Session() as session:
+                    url = 'https://www.statuto18.com/DunkHighMaizeBlue'
 
-                r = session.post(url, data=m, headers=headers2, proxies=proxies)
+                    res = requests.get(url, proxies=proxies, headers=headers1)
+                    soup = BeautifulSoup(res.text, 'html.parser')
 
-                soup = BeautifulSoup(r.text, 'lxml')
-                thank = soup.find("div", id="content-thankyou")
-                if thank==None:
-                    print(row['Mail'], "non entrata")
-                    text_file = open("ST1.txt", "a")
-                    text_file.write(row['Cognome'])
-                    text_file.write('\n')
-                    text_file.close()
-                else:
-                    print(row['Mail'], "DENTRO")
+                    day = soup.select_one('input.form-control:nth-child(3)').get('name')
+                    month = soup.select_one('input.form-control:nth-child(4)').get('name')
+                    year = soup.select_one('input.form-control:nth-child(5)').get('name')
+                    cf = soup.select_one('input.form-control:nth-child(9)').get('name')
+
+                    if not day or not month or not year or not cf:
+                        print("Empty field: day/month/year/cf")
+                        continue
+
+
+                    # solver = recaptchaV2Proxyless()
+                    # solver.set_verbose(1)
+                    # solver.set_key("bef80b4f70976b0452b83cddf6f9b152")
+                    # solver.set_website_url(url)
+                    # solver.set_website_key("6LdrtrgUAAAAAAAio3UhHrVdJUQXpP3vfbcFm3qx")
+                    # g_response = solver.solve_and_return_solution()
+                    # if g_response != 0:
+                    #     print("g-response: " + g_response)
+                    # else:
+                    #     print("task finished with error " + solver.error_code)
+
+                    while queue.empty():
+                        time.sleep(0.1)
+                    g_response = queue.get()
+                    form_data = {
+                        'nome2': row['Nome'],
+                        'cognome2': row['Cognome'],
+                        day: row['Giorno'],
+                        month: row['Mese'],
+                        year: row['Anno'],
+                        'telefono2': row['Telefono'],
+                        'email2': row['Mail'],
+                        'email_conf2': row['Mail'],
+                        cf: row['CF'],
+                        'comune2': row['Comune'],
+                        'provincia2': row['Provincia'],
+                        'paese2': 'IT',
+                        'sesso2': row['Sesso'],
+                        'taglia2': row['Taglia'],
+                        'check2': 'S',
+                        'captcha2': 'undefined',
+                        'campagna2': 'DunkHighMaizeBlue',
+                        'form': 'registra',
+                        'mark': 'S',
+                        'cessione': 'S',
+                        'g-recaptcha-response': g_response,
+                        'action': 'trueins'
+                    }
+
+                    m = MultipartEncoder(fields=form_data, boundary='----WebKitFormBoundary' + ''.join(
+                        random.sample(string.ascii_letters + string.digits, 16)))
+
+                    headers2 = {
+                        "Content-Type": m.content_type,
+                        'Cookie': 'PHPSESSID=' + res.cookies.get('PHPSESSID'),
+                        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
+                    }
+
+                    # print(form_data)
+
+                    r = session.post(url, data=m, headers=headers2, proxies=proxies)
+
+                    soup = BeautifulSoup(r.text, 'lxml')
+                    thank = soup.find("div", id="content-thankyou")
+                    if thank==None:
+                        print(row['Mail'], "non entrata")
+                        text_file = open("ST1.txt", "a")
+                        text_file.write(row['Cognome'])
+                        text_file.write('\n')
+                        text_file.close()
+                    else:
+                        print(row['Mail'], "DENTRO")
+            except:
+                print ("_______è andato tutto a puttane_________")
+        
 
 
 
